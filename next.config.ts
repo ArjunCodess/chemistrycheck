11c84670c1c541cc19290f8441e654da98d9f9c5
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2026-02-15 09:32:58 +0530

    remove logging from production

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
-}
+};
 
 export default nextConfig;
